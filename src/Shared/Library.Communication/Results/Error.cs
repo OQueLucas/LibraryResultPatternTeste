363a133522ea4ffaac1763674069c4f5d87b5e69
OQueLucas <l_queiroz@outlook.com.br>
2024-08-06 21:13:17 -0300

    Update Error class with override to receive error list

--- src/Shared/Library.Communication/Results/Error.cs
+++ src/Shared/Library.Communication/Results/Error.cs
@@ -5,15 +5,22 @@ public record Error
     public static readonly Error None = new(string.Empty, string.Empty, ErrorType.Failure);
     public static readonly Error NullValue = new("Error.NullValue", "Null Value was providad", ErrorType.Failure);
 
-    public Error(string code, string description, ErrorType type)
+    public Error(string code, string error, ErrorType type)
     {
         Code = code;
-        Description = description;
+        Errors = [error];
+        Type = type;
+    }
+
+    public Error(string code, List<string> errors, ErrorType type)
+    {
+        Code = code;
+        Errors = errors;
         Type = type;
     }
 
     public string Code { get; }
-    public string Description { get; }
+    public List<string> Errors { get; }
     public ErrorType Type { get; }
 
     public static Error NotFound(string code, string description) =>
@@ -21,6 +28,9 @@ public record Error
 
     public static Error Validation(string code, string description) =>
         new(code, description, ErrorType.Validation);
+
+    public static Error Validation(string code, List<string> errors) =>
+        new(code, errors, ErrorType.Validation);
 
     public static Error Conflict(string code, string description) =>
         new(code, description, ErrorType.Conflict);
