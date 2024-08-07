b8cf2dbafcdd0ef4c98db8238023aece32f12484
OQueLucas <l_queiroz@outlook.com.br>
2024-08-06 21:11:34 -0300

    add errors messages class

---
+++ src/Shared/Library.Communication/MessagesError/ErrorMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.Communication.MessagesError;
+
+public class ErrorMessages
+{
+    public static string AUTHOR_EMPTY() => "The Author cannot be empty.";
+    public static string ISBN_EMPTY() => "The ISBN cannot be empty.";
+    public static string NAME_EMPTY() => "The Name cannot be empty.";
+    public static string TITLE_EMPTY() => "The Title cannot be empty.";
+}
